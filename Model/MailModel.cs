3cf51c8a6e8510675cbc37a8b949722808bdc277
Szymon <szymzwoo@gmail.com>
2022-10-24 13:55:54 +0200

    Dodanie wzorca MVVC, modeli i widoku głownego

---
+++ Model/MailModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botex.Model
+{
+    internal class MailModel
+    {
+        private int idMail;
+        private string content;
+        private string title;
+        private string group;
+
+        public int IdMail { get { return idMail; } set { idMail = value; } }
+        public string Content { get { return content; } set { content = value; } }
+        public string Title { get { return title; } set { title = value; } }
+        public string Group { get { return group; } set { group = value; } }
+    }
+}
---
+++ Model/TweetModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botex.Model
+{
+    internal class TweetModel
+    {
+        private int idTweet;
+        private string content;
+        private string group;
+
+        public int IdTweet { get { return idTweet; } set { idTweet = value; } }
+        public string Content { get { return content; } set { content = value; } }
+        public string Group { get { return group; } set { group = value; } }
+    }
+}
